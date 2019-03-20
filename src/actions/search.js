96e5a364ac72fa823220c55a60ffed2f6ab6286b
Victor Liu <victorliu716@gmail.com>
2019-03-20 16:14:30 -0700

    Call youtube api

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,11 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  return function() {
+    type: searchYouTube((YOUTUBE_API_KEY, q, 5), () => {
+    })
+  }
 };
 
 export default handleVideoSearch;
